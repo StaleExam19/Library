836bd48f71165bb8f263ced8af6d29b28d76e665
StaleExam19 <staleexam19@gmail.com>
2022-05-16 17:20:10 +0800

    Initial commit

--- src/ready.ts
+++ src/ready.ts
@@ -6,13 +6,13 @@ import { ind, addBookForm, modalContainer, overlay } from "./components";
 let books = localStorage.getItem("books");
 
 
-if ((<Book[]>JSON.parse(books!)).length !== 0) {
-    ind?.classList.add("hidden");
-    updateBookContainer();
-}
-
 if (!books) {
     localStorage.setItem("books", JSON.stringify([]));
     books = localStorage.getItem("books");
     ind?.classList.remove("hidden");
+}
+
+if ((<Book[]>JSON.parse(books!)).length !== 0) {
+    ind?.classList.add("hidden");
+    updateBookContainer();
 }
